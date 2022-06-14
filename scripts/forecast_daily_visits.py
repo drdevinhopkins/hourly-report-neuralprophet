ca1a9ac6b6f20afc499dfca41df8354905e3b940
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2022-06-14 14:48:40 -0400

    Update forecast_daily_visits.py

--- scripts/forecast_daily_visits.py
+++ scripts/forecast_daily_visits.py
@@ -15,10 +15,10 @@ future = loaded_model.make_future_dataframe(df, periods=8) # periods=m.n_forecas
 forecast = loaded_model.predict(future, raw=True, decompose=False)
 
 start = forecast.values.tolist()[-1][0]
-forecast_length = len(forecast.values.tolist()[0][1:])
+forecast_length = len(forecast.values.tolist()[-1][1:])
 
 forecast_output = pd.DataFrame(columns=['ds','visits','timestamp'])
 forecast_output['ds'] = pd.date_range(start=start, periods=forecast_length, freq='D')
-forecast_output['visits'] = forecast.values.tolist()[0][1:]
+forecast_output['visits'] = forecast.values.tolist()[-1][1:]
 forecast_output['timestamp'] = pd.Timestamp.now().round('S').replace(tzinfo=None)
 forecast_output.to_csv('forecasts/daily_visits.csv', index=False)
