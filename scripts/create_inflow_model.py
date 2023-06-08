1eb36e17ab70ed32514641136905cc8bac72ecfe
drdevinhopkins <41838748+drdevinhopkins@users.noreply.github.com>
2023-06-08 10:18:10 -0400

    Update create_inflow_model.py

--- scripts/create_inflow_model.py
+++ scripts/create_inflow_model.py
@@ -16,8 +16,10 @@ set_log_level("ERROR")
 
 target_column = 'Total Inflow hrly'
 
+# df = pd.read_csv(
+#     'https://raw.githubusercontent.com/drdevinhopkins/hourly-report/main/data/since-2020.csv')
 df = pd.read_csv(
-    'https://raw.githubusercontent.com/drdevinhopkins/hourly-report/main/data/since-2020.csv')
+    'data/since-2020.csv')
 df.ds = pd.to_datetime(df.ds)
 df = df.drop(['Date', 'Time'], axis=1)
 df = df.sort_values(by='ds', ascending=True)
